90f521062c910f18892c2d65de60156d17c93bdf
khayalfarzi <khayal.farziyev@pashabank.az>
2021-09-15 13:51:08 +0400

    lessons: lesson 30 lesson

---
+++ src/main/java/az/coders/ada_students/lessons/lesson_30/ThreadExample.java
@@ -0,0 +1,11 @@
+package az.coders.ada_students.lessons.lesson_30;
+
+public class ThreadExample implements Runnable{
+
+    public ThreadExample() {
+    }
+
+    @Override
+    public void run() {
+    }
+}
